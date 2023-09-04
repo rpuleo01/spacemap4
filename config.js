f9b2ba4bd5c5095d85fda4fbfa7550d8fb9d3a0f
rpuleo01 <138991128+rpuleo01@users.noreply.github.com>
2023-09-04 14:04:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "SpaceMap.gexf",
+    graphFile : "#SpaceMap.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
